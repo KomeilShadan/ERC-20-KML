6c30dd05d04b61383c53ed7bd1dae42859dab8dc
Komeil <k.shadannejad@doctoreto.com>
2024-07-06 17:07:43 +0330

    update hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: process.env.MORALIS_TESTNET_NODE,
+      accounts: [process.env.PRIVATE_KEY]  
+    }
+  },
+  etherscan: {
+    apiKey: process.env.MORALIS_API_KEY
+  }
 };
